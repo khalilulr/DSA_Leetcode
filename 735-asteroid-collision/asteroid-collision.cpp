900f5a6030970286f7db5bedb80a7288c6fd122e
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-07-20 11:48:37 +0530

    Time: 3 ms (28.54%) | Memory: 22 MB (60.58%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        //-> <- (+ -)
+        //-> ->
+        //<- ->
+        //<- <-
         stack<int>st;
         for(auto ast:asteroids){
-            bool isAlive=true;
-            while(isAlive &&
-                !st.empty()
-                && st.top()>0
-                && ast<0){
+                bool isAlive=true;
+                while(isAlive && !st.empty() && st.top()>0 &&  ast<0){
                     if(st.top()<-ast){
                         st.pop();
-                    }
-                    else if(st.top()==-ast){
+                    }else if(st.top()==-ast){
                         st.pop();
                         isAlive=false;
-                    }
-                    else isAlive=false;
+                    }else
+                        isAlive=false;
                 }
-            if(isAlive)
-                st.push(ast);
+                if(isAlive)
+                    st.push(ast);
+                
         }
         vector<int>ans;
         while(!st.empty()){
@@ -26,6 +27,7 @@ public:
             st.pop();
         }
         reverse(ans.begin(),ans.end());
+
         return ans;
     }
 };
