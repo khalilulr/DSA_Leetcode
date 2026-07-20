352a54c35d7aa5bf36cf8f60e151a7f38a9ecc25
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-07-20 11:30:19 +0530

    Time: 5 ms (20.13%) | Memory: 8.6 MB (21.06%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int num=0;
+        while(x){
+            int rem=x%10;
+            if(num>INT_MAX/10)
+                return 0;
+            if(num<INT_MIN/10)
+                return 0;
+            num=num*10 + rem;
+            x=x/10;
+        }
+        return num;
+    }
+};
