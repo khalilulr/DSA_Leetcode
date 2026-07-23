673af28a6a2a9df83bc850336b5ab910f961c501
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-07-23 23:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.51%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l = 0;
-        int h = nums.size() - 1;
-        int ans = nums.size();
-
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-
-            if (nums[mid] >= target) {
-                ans = mid;      // possible answer
-                h = mid - 1;    // search left for first occurrence
-            } else {
-                l = mid + 1;
-            }
-        }
-
+       int ans=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
         return ans;
     }
 };
