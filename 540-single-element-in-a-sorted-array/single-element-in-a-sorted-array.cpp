7773eeaae77b26a863afa66476bd9268b1bc6da1
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-07-27 12:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (35.69%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //1,1,2,3,3,4,4,8,8
-        //(0 1) (3 4) (5 6) (7 8)
-        //midIdx=4 idx(4== 3) (0..2) (5..8)
-        //0 1 2    5 6 7 8
+        // all the elem dublicate except one
+        //(0,1) (1,2) ... (n-2,n-1);
+        // 0......x......n-1
+        //at x idx, x-1 idx value are same
+        //ideal case
+        //x->odd idx and x-1 is obv even idx
+        //x->even idx and x-1 is obv odd idx
 
-        //3 3 7 8 8
+        //if x idx is even && x+1 is the next same value as of x idx  || x idx is odd and x-1 is 
+
+
 
         int n=nums.size();
-        if(n==1)
+          if(n==1)
             return nums[0];
 
         if(n>1){
@@ -19,20 +24,16 @@ public:
             if(nums[n-2]!=nums[n-1])
                 return nums[n-1];
         }
-
-        int l=0,h=n-1;
+        int l=0,h=n-1,ans=-1;
 
         while(l<=h){
             int mid=l + (h-l)/2;
-
-            if(nums[mid-1]!=nums[mid]  && nums[mid]!=nums[mid+1])
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1])
                 return nums[mid];
-
-            if((mid%2!=0 && nums[mid]==nums[mid+1]) || (mid%2==0 && nums[mid-1]==nums[mid]))
+            if((mid%2==1 && nums[mid+1]==nums[mid]) || (mid%2==0 && nums[mid-1]==nums[mid]))
                 h=mid-1;
             else l=mid+1;
         }
-
-        return -1;
+            return -1;
     }
 };
