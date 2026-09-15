a218fd435e60362386d6bc17efa2e46e8b212f29
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-09-15 09:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (79.76%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // x y | z
-        // p q | r
 
-        //y<r && z>q
-        //y>r==> 
-
-
+        //  . . a  b . .
+        //  . . c  d 
         int n=nums1.size(),m=nums2.size();
         if(n>m)
-           return findMedianSortedArrays(nums2,nums1);
+            return findMedianSortedArrays(nums2,nums1);
 
-        int totalMid= (n + m + 1) /2;
+        int size = n + m ;
+        int midSize = (size+1) / 2;
+
         int l=0,h=n;
 
         while(l<=h){
-            int mid1=l + (h-l)/2;
-            int mid2=totalMid-mid1;
+            int mid1=l + (h-l) / 2 ;
+            int mid2=midSize - mid1;
 
-            int y=mid1==0?INT_MIN:nums1[mid1-1];
-            int z=mid1==n?INT_MAX:nums1[mid1];
-            int q=mid2==0?INT_MIN:nums2[mid2-1];
-            int r=mid2==m?INT_MAX:nums2[mid2];
+            int a=mid1!=0?nums1[mid1-1]:INT_MIN;
+            int b=mid1!=n?nums1[mid1]:INT_MAX;
 
+            int c=mid2!=0?nums2[mid2-1]:INT_MIN;
+            int d=mid2<m?nums2[mid2]:INT_MAX;
 
-            if(y<=r && z>=q){
-                if((n+m)%2==0){
-                    return double(max(y,q) + min(z,r)) /2.0;
-                }else{
-                    return max(y,q)/1.0;
-                }
+            if(a<=d && b>=c){
+                return size%2==0?(max(a,c)+ min(b,d)) / 2.0 : max(a,c) ;
             }
 
-            if(y>r)
+            if(a>d){
                 h=mid1-1;
-            else l=mid1+1;
+            }else l=mid1+1;
         }
+
         return 0.00;
+
     }
 };
